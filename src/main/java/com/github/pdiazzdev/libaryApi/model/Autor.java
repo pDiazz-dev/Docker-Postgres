9115b80a4e97de86b024eea2b6c9ba8703501dc8
pDiazz-dev <pedrodias20152015@gmail.com>
2025-06-23 16:33:11 -0300

    adcionando entidade Autor, para a tabela autor do db

---
+++ src/main/java/com/github/pdiazzdev/libaryApi/model/Autor.java
@@ -0,0 +1,34 @@
+package com.github.pdiazzdev.libaryApi.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.context.annotation.Primary;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "autor" , schema = "public") //quanod for public não precisa colocar.
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Autor {
+
+    @Id
+    @Column(name = "id") // preicisa ser o mesmo nome da tabala do banco
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "nome", length = 100, nullable = false)
+    private String nome;
+
+    @Column(name = "data_nascimento", nullable = false)
+    private LocalDate dataNascimento;
+
+    @Column(name = "nacionalidade", length = 50, nullable = false)
+    private String nacionalidade;
+}
